0a3b0c9b91296cbdc2d4508fcdc3415d403964e8
Zhulieta Yaneva <zhulieta.yaneva4@gmail.com>
2022-03-02 18:20:45 +0200

    Working function to show currencies for individual users. Fixed multiple open DataReaders error.

--- BankProj/Form1.cs
+++ BankProj/Form1.cs
@@ -18,39 +18,31 @@ namespace BankProj
 
         public static string connString = "SERVER=localhost;PORT=3306;DATABASE=bank;UID=root";
         public static MySqlConnection conn = new MySqlConnection(connString);
+        public static MySqlDataReader reader;
 
 
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-
             if (conn.State.ToString() != "Open")
             {
-
                 try
                 {
                     conn.Open();
                     button1.BackColor = Color.LightGreen;
                     button1.Text = $"Connected to DB {conn.Database}";
-
                 }
                 catch (Exception)
                 {
-
                     button1.Text = "Ne raboti";
                 }
-
             }
             else
             {
                 button1.BackColor = Color.LightPink;
                 conn.Close();
-                button1.Text = "Connect to DB";
-               
-
+                button1.Text = "Connect to DB";    
             }
-
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -61,14 +53,37 @@ namespace BankProj
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MySqlCommand cmd = new MySqlCommand("select * from currency");
+            MySqlCommand cmd = new MySqlCommand("select * from currency", conn);
+            EasyPrint(cmd);
+        }
 
+        private void EasyPrint(MySqlCommand cmd)
+        {
+            
 
+            DataTable dt =new DataTable();            
+            reader = cmd.ExecuteReader();
+            dt.Load(reader);            
+            dataGridView1.DataSource = dt;
+        }
 
+        private bool ReaderRows(MySqlCommand cmd)
+        {   
+            reader = cmd.ExecuteReader();
+            reader.Close();
+            return reader.HasRows;
+        }
+        
+        //----------------------------------------------
+                //UNREASONABLY LONG PRINT METHOD//
+        //----------------------------------------------
+        /*
+        private void PrintOnTable(MySqlCommand cmd)
+        {
             try
             {
-                MySqlDataAdapter sda = new MySqlDataAdapter(cmd.CommandText,conn);
-                
+                MySqlDataAdapter sda = new MySqlDataAdapter(cmd.CommandText, conn);
+
                 DataTable dbdataset = new DataTable();
                 sda.Fill(dbdataset);
                 BindingSource bSource = new BindingSource();
@@ -80,18 +95,12 @@ namespace BankProj
             {
                 MessageBox.Show(ex.Message);
             }
-
-
-
-
-
         }
-
+        */
         private void button3_Click(object sender, EventArgs e)
         {
             dataGridView1.DataSource = null;
             dataGridView1.Rows.Clear();
-
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -101,15 +110,24 @@ namespace BankProj
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if (conn.State.ToString()!="Open")
+            {
+                conn.Open();
+            }
             var userid = textBox1.Text.Trim();
-            MySqlCommand cmd = new MySqlCommand($"select * from assets where user_id={userid}");
+            MySqlCommand cmd = new MySqlCommand($"select * from assets where user_id={userid}",conn);
+
             if (userid == String.Empty)
             {
                 MessageBox.Show("Please enter User ID", "User ID error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else if(cmd.ExecuteNonQuery()==0)
+            else if(ReaderRows(cmd))
             {
                 MessageBox.Show("Please enter a valid User ID", "User ID error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                EasyPrint(cmd);
             }
         }
     }
